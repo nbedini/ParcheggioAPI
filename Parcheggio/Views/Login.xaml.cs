abd42a0a2f362b4a60174dc4a26686c99ebdfc1d
Elia Capiluppi <eliacapiluppi02gmail.com>
2021-12-02 16:20:22 +0100

    login fix

--- Parcheggio/Views/Login.xaml.cs
+++ Parcheggio/Views/Login.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,14 +21,32 @@ namespace Parcheggio.Views
     /// </summary>
     public partial class Login : Window
     {
+        static HttpClient client = new HttpClient();
         public Login()
         {
             InitializeComponent();
         }
 
-        private void SubmitClick(object sender, RoutedEventArgs e)
+        private async void SubmitClick(object sender, RoutedEventArgs e)
         {
-
+            string username = tbUsername.Text;
+            string password = tbPassword.Password;
+            object candidato = new { username = username, password = password };
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("http://localhost:31329/api/Login"),
+                Content = new StringContent(JsonConvert.SerializeObject(candidato), Encoding.UTF8, "application/json")
+            };
+            var response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Login andato a buon fine");
+            }
+            else
+            {
+                MessageBox.Show("Username e password non coincidenti");
+            }
         }
 
         private void RegistrazioneClick(object sender, RoutedEventArgs e)
