8bf78d7faa15bd2e10a90e85009b24eabed241b0
Blear Hoxha <blear.hoxha95@gmail.com>
2021-12-06 16:35:11 +0100

    creazione progetto asp net core per visualizzazione web proprietari macchine

---
+++ ParcheggioWeb/Controllers/ProprietarioController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParcheggioAPI.Models;
+
+namespace ParcheggioWeb.Controllers
+{
+    public class ProprietarioController : Controller
+    {
+        [HttpGet]
+        [Route("/VisualizzaProprietario")]
+        public IActionResult Index(string CodiceFiscale)
+        {
+            if (CodiceFiscale == null) return Problem("Codice fiscale attualmente non esistente nel registro dati");
+            using(ParkingSystemContext model = new ParkingSystemContext())
+            {
+                var proprietario = model.Persons.FirstOrDefault(o=>o.CodiceFiscale.Equals(CodiceFiscale));
+                if (proprietario == null) return NotFound("Proprietario non trovato");
+                return View(proprietario);
+            }
+        }
+    }
+}
