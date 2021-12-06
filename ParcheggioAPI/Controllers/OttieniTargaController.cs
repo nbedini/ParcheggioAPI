8b2e71893a204a2cc83a84567312da7db01880c7
Blear Hoxha <blear.hoxha95@gmail.com>
2021-12-06 11:50:35 +0100

    passaggio oggetto anonimo in ottieni targa controller

--- ParcheggioAPI/Controllers/OttieniTargaController.cs
+++ ParcheggioAPI/Controllers/OttieniTargaController.cs
@@ -20,7 +20,11 @@ namespace ParcheggioAPI.Controllers
                     .FirstOrDefault(fod => fod.Riga == riga && fod.Colonna == colonna && fod.NomeParcheggio == nomeParcheggio);
                 if (veicolo != null)
                 {
-                    return Ok(veicolo.Targa);
+                    return Ok(new
+                    {
+                        Targa = veicolo.Targa,
+                        TipoVeicolo = veicolo.TipoVeicolo
+                    });
                 }
                 else
                     return NotFound(null);
