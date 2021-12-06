72dcee7130fd0d2a6db436c28cb1989501f810fb
Blear Hoxha <blear.hoxha95@gmail.com>
2021-12-06 11:25:42 +0100

    controllo ottieni targa

---
+++ ParcheggioAPI/Controllers/OttieniTargaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParcheggioAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParcheggioAPI.Controllers
+{
+    [ApiController]
+    public class OttieniTargaController : ControllerBase
+    {
+        [HttpGet("/api/ottienitarga/{riga}/{colonna}/{nomeParcheggio}")]
+        public static string OttenimentoTarga(string riga, string colonna, string nomeParcheggio)
+        {
+            using (ParkingSystemContext model = new ParkingSystemContext())
+            {
+                ParkingStatuss veicolo = model.ParkingStatusses
+                    .FirstOrDefault(fod => fod.Riga == riga && fod.Colonna == colonna && fod.NomeParcheggio == nomeParcheggio);
+                if (veicolo != null)
+                {
+                    return veicolo.Targa;
+                }
+                else
+                    return null;
+            }
+        }
+    }
+}
