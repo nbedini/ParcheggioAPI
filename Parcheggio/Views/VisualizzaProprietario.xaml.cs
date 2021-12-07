2191b7de40c21f135aa8ced76187de91d5ba720e
Nikolas Bedini <nikolasbedini04@gmail.com>
2021-12-07 10:40:56 +0100

    visualizza proprietario view e funzionamento

---
+++ Parcheggio/Views/VisualizzaProprietario.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Parcheggio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Parcheggio.Views
+{
+    /// <summary>
+    /// Logica di interazione per VisualizzaProprietario.xaml
+    /// </summary>
+    public partial class VisualizzaProprietario : Window
+    {
+        public ParkingHistory SelectedItem { get; set; }
+        public VisualizzaProprietario()
+        {
+            InitializeComponent();
+            SelectedItem = VisualizzaStorico.SelectedItem;
+            WebBrowser webBrowser = new WebBrowser();
+            webBrowser.Source = new Uri($"http://localhost:34483/VisualizzaProprietario/{SelectedItem.Propietario}");
+            GridDefault.Children.Add(webBrowser);
+            this.DataContext = this;
+        }
+    }
+}
---
+++ ParcheggioAPI/Controllers/StoricoPacheggioController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParcheggioAPI.Models;
+using System.Linq;
+
+namespace ParcheggioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoricoPacheggioController : ControllerBase
+    {
+        [HttpGet("/api/storicoparcheggio/{nomeParcheggio}")]
+        public ActionResult StoricoParcheggio(string nomeParcheggio)
+        {
+            using (ParkingSystemContext model = new ParkingSystemContext())
+            {
+                var Veicoli = model.ParkingHistorys.Where(o => o.NomeParcheggio == nomeParcheggio).ToList();
+                if (Veicoli.Count() > 0)
+                    return Ok(Veicoli);
+                else
+                    return Problem();
+            }
+        }
+
+        [HttpPost("/api/storicoricercatarga")]
+        public ActionResult StoricoRicercaTarga([FromBody] RicercaTarga targaeparcheggio)
+        {
+            using (ParkingSystemContext model = new ParkingSystemContext())
+            {
+                var filtro = model.ParkingHistorys.Where(o => o.Targa.Contains(targaeparcheggio.targaparziale) && o.NomeParcheggio == targaeparcheggio.NomeParcheggio).ToList();
+                if (filtro != null)
+                    return Ok(filtro);
+                else
+                    return NotFound();
+            }
+        }
+    }
+}
