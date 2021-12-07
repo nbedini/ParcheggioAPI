4b1ae5ed9bfcd40bc09d858cf28659fd71e15419
Alessandro Rondanini <rondanini.alle@gmail.com>
2021-12-07 10:28:15 +0100

    Inizio Incassi

---
+++ ParkingWeb/Controllers/IncassiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkingWeb.Model;
+using ParkingWeb.Models;
+
+namespace ParkingWeb.Controllers
+{
+    public class IncassiController : Controller
+    {
+        [HttpGet]
+        [Route("/Incassi")]
+        public IActionResult Index()
+        {
+            List<IncassiGornalieri> incassi = new List<IncassiGornalieri>();
+            return View(incassi);
+        }
+    }
+}
---
+++ ParkingWeb/Models/IncassiGiornalieri.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkingWeb.Model;
+namespace ParkingWeb.Models
+{
+    public class IncassiGornalieri
+    {
+        public List<ParkingAmount> Incassi { get; set; }
+    }
+}
